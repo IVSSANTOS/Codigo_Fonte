1ca36b947eb2533d9327fadec411131d3c3ac0c2
Ivan Santos <souzasantos.ivan@gmail.com>
2019-02-15 09:25:57 -0200

    FiltroSimplesNacional

--- 01-Apresentacao/PrefeituraConecta.MVC.UI/Models/FiltroSimplesNacionalModel.cs
+++ 01-Apresentacao/PrefeituraConecta.MVC.UI/Models/FiltroSimplesNacionalModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,20 @@ namespace PrefeituraConecta.MVC.UI.Models
 {
     public class FiltroSimplesNacionalModel
     {
+
+        public int ID_CONSULTA { get; set; }
+
+        [Display(Name = "Consulta)")]
+        public string Consulta { get; set; }
+
+        [Display(Name = "Consolidado por")]
+        public string ConsolidadoPor { get; set; }
+
+        [Display(Name = "Tipo de Empresa")]
+        public string TipoEmpresa { get; set; }
+
+        [Display(Name = "Tipo da Declaração")]
+        public string TipoDeclaracao { get; set; }
+
     }
 }
